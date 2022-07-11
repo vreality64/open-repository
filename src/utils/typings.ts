0a16f23ede9481e1607de8091639c12f9083f05d
vreality64 <sh.park@toss.im>
2022-07-11 14:25:25 +0900

    feat: validator utils 함수 추가

---
+++ src/utils/typings.ts
@@ -0,0 +1,5 @@
+export type Nil = null | undefined;
+
+export type Nullable<T> = T | null;
+
+export type Nilable<T> = Nullable<T> | undefined;
---
+++ src/utils/validator.ts
@@ -0,0 +1,13 @@
+import { Nil, Nilable } from './typings';
+
+export function isNil<T>(value: Nilable<T>): value is Nil {
+  return value == null;
+}
+
+export function hasPositiveLength(value: { length: number }) {
+  return value.length > 0;
+}
+
+export function isEmptyStringOrNil(value: unknown): value is Nil | '' {
+  return isNil(value) || (typeof value === 'string' && !hasPositiveLength(value));
+}
